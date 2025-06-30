48f15676f86adc1e1b09ff93606e74781d2b9fa0
YisharaU <wellahewawiud.20@uom.lk>
2025-06-30 15:32:17 +0530

    Add WebFlux dependencies and refactor GenerateEmailService to use WebClient

--- assistant/src/main/java/lk/email/assistant/service/GenerateEmailService.java
+++ assistant/src/main/java/lk/email/assistant/service/GenerateEmailService.java
@@ -2,14 +2,21 @@ package lk.email.assistant.service;
 
 import lk.email.assistant.entity.EmailRequest;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Map;
 
 public class GenerateEmailService {
+    private final WebClient webClient;
+
     @Value("${gemini.api.url}")
     private String geminiApiUrl;
     @Value("${gemini.api.key}")
     private String geminiKey;
+
+    public GenerateEmailService(WebClient webClient) {
+        this.webClient = webClient;
+    }
 
     public String generateReply(EmailRequest emailRequest) {
         String prompt = buildPrompt(emailRequest);
